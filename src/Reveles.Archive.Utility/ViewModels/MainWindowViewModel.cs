e94be3d081342dd14793d3b03e2039e811fca433
Mikhail Burilov <burilovmv@gmail.com>
2021-11-10 18:22:24 +0300

    ui mockup

---
+++ src/Reveles.Archive.Utility/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+
+namespace Reveles.Archive.Utility.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MainWindowViewModel
+    {
+        public int State { get; set; }
+        public string FileName { get; set; }
+    }
+}
