ab46f44f247068726fa2e8e8af98cf86f7440e0c
Mikhail Burilov <burilovmv@gmail.com>
2021-11-11 15:11:37 +0300

    Fixed issue with value binding

--- src/Reveles.Archive.Utility/ViewModels/MainWindowViewModel.Settings.cs
+++ src/Reveles.Archive.Utility/ViewModels/MainWindowViewModel.Settings.cs
@@ -9,8 +9,14 @@ namespace Reveles.Archive.Utility.ViewModels
 {
     public partial class MainWindowViewModel
     {
-        public ObservableCollection<string> CloudProviders = new ObservableCollection<string>();
+        public ObservableCollection<string> CloudProviders { get; } = new ObservableCollection<string>();
 
+        public int SelectedProviderIndex { get; set; }
 
+        public bool DeleteFilesAfterUpload { get; set; } = true;
+
+        public int MaximumFiles { get; set; } = 1000;
+
+        public int MaximumArchiveSizeMb { get; set; } = 2048;
     }
 }
